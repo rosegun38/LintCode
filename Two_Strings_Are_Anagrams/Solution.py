ee57f25e4769925821235f0a06db00e92c279e01
Jian.Ma <jian.ma@dianrong.com>
2016-02-03 15:57:13 +0800

    one solution for just lower case Character.

--- Two_Strings_Are_Anagrams/Solution.py
+++ Two_Strings_Are_Anagrams/Solution.py
@@ -6,3 +6,23 @@ class Solution:
     """
     def anagram(self, s, t):
         # write your code here
+        c1 = [0] * 26
+        c2 = [0] * 26
+
+        for i in range(len(s)):
+            pos = ord(s[i]) - ord('a')
+            c1[pos] += 1
+
+        for i in range(len(t)):
+            pos = ord(t[i]) - ord('a')
+            c2[pos] += 1
+
+        j = 0
+        stillOK = True
+        while j < 26 and stillOK:
+            if c1[j] == c2[j]:
+                j += 1
+            else:
+                stillOK = False
+
+        return stillOK
